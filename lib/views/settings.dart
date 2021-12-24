b504c863f81e1c10b864deb38a05cccbbe705f42
0eta0 <eta.develop@gmail.com>
2021-12-24 14:26:46 +0900

    feat: add compo in settings

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -6,10 +6,26 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        ListTile(
+      children: [
+        const ListTile(
           leading: Icon(Icons.lightbulb),
           title: Text("Dark/Light Mode"),
+        ),
+        SwitchListTile(
+          title: const Text("Switch"),
+          value: true,
+          onChanged: (yes) => {}
+        ),
+        CheckboxListTile(
+          title: const Text("Checkbox"),
+          value: true,
+          onChanged: (yes) => {}
+        ),
+        RadioListTile(
+          title: const Text("Radio"),
+          value: true,
+          groupValue: true,
+          onChanged: (yes) => {},
         ),
       ],
     );
